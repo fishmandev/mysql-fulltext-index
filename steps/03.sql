1a5be58642786aba7901361237443b23e7f65902
Dmitriy Fishman <fishman.code@gmail.com>
2022-06-16 00:53:01 +0300

    Generated data procedure

---
+++ steps/03.sql
@@ -0,0 +1,16 @@
+DROP PROCEDURE IF EXISTS generate;
+DELIMITER //
+CREATE PROCEDURE generate()
+BEGIN
+DECLARE i INT DEFAULT 0;
+WHILE (i <= 100) DO
+    INSERT INTO `search`
+        SET f1=CONCAT(lipsum(2000, 2000, 0)),
+            f2=CONCAT(lipsum(2000, 2000, 0)),
+            f3=CONCAT(lipsum(2000, 2000, 0)),
+            f4=CONCAT(lipsum(2000, 2000, 0)),
+            f5=CONCAT(lipsum(2000, 2000, 0));
+    SET i = i+1;
+END WHILE;
+END;
+//
